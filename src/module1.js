64b01fd867ad81365878ff5af332f73afe5ae15a
Zacky <mr.kaz404@gmail.com>
2020-03-31 19:52:02 +0900

    yamazaki_study_0329

---
+++ src/module1.js
@@ -0,0 +1,5 @@
+const module1 = () => {
+    console.log("module1の関数")
+}
+
+export default module1
---
+++ src/module2.js
@@ -0,0 +1,5 @@
+const module2 = () => {
+    console.log("module2の関数更新")
+}
+
+export default module2
